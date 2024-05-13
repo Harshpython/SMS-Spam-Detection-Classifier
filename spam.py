41496c60a19c0ac7fdc21d35e4ca6246f6577760
Harshdeep Singh <140813105+Harshpython@users.noreply.github.com>
2024-05-13 10:36:58 +0530

    Update spam.py

--- spam.py
+++ spam.py
@@ -6,21 +6,21 @@ import pickle
 app = Flask(__name__)
 
 # Load the trained model and vectorizer
-with open('model.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f:File open
     model = pickle.load(f)# loading the module
-with open('vectorizer.pkl', 'rb') as f:
+with open('vectorizer.pkl', 'rb') as f:#vectorisation done
     vectorizer = pickle.load(f)
 
 
 # Define a route for the homepage
-@app.route('/')
+@app.route('/')#route of the site
 def home():
     return render_template('index.html')
 
 
 # Define a route for predicting email spam
 @app.route('/predict', methods=['POST'])
-def predict_spam():
+def predict_spam():# predicting wheather the mail is spam or not
     # Get the email text from the request
     email_text = request.form['email_text']
 
@@ -28,11 +28,11 @@ def predict_spam():
     X_test = vectorizer.transform([email_text])# training the dataset
 
     # Predict the spam probability using the trained model
-    spam_probability = model.predict_proba(X_test)[:, 1].item()
+    spam_probability = model.predict_proba(X_test)[:, 1].item()# having the probability of the mail is spam or not
 
     # Return the predicted spam probability
     return render_template('index.html', spam_probability=spam_probability, email_text=email_text)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)# main function
